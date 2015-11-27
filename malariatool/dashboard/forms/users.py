122209cb2aad26891a42616ef5ad121768551b5a
Remo Samuel Paul <remosam42@gmail.com>
2015-11-27 08:38:39 +0300

    styling forms

--- malariatool/dashboard/forms/users.py
+++ malariatool/dashboard/forms/users.py
@@ -6,4 +6,4 @@ from dashboard.models import User
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'password', 'email']
+        fields = ['title', 'first_name', 'last_name', 'password', 'email']
