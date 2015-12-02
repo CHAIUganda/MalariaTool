32c9993e501ece7eaa4cec3468c3648be7a380c3
Remo Samuel Paul <remosam42@gmail.com>
2015-12-02 08:02:36 +0300

    implemented types of documents

--- malariatool/dashboard/models/document.py
+++ malariatool/dashboard/models/document.py
@@ -5,8 +5,13 @@ from dashboard.models.user import User
 
 
 class Document(TimeStampedModel):
+    type_choices = (
+        ('Policy', 'Policy'),
+        ('Guideline', 'Guideline'),
+        ('Manual', 'Manual'),
+    )
     display_name = models.CharField(max_length=255)
     description = models.TextField()
     file = models.FileField(upload_to='documents/%Y/%m/%d')
-    type = models.CharField(max_length=50)
+    type = models.CharField(max_length=50, choices=type_choices)
     uploader = models.ForeignKey(User, related_name='uploader')
--- malariatool/dashboard/views/document.py
+++ malariatool/dashboard/views/document.py
@@ -10,16 +10,11 @@ class DocumentListView(ListView):
 
 class DocumentCreateView(CreateView):
     model = Document
-    fields = ['display_name', 'file', 'description']
+    fields = ['display_name', 'file', 'description','type']
     success_url = reverse_lazy("dashboard:document-list")
 
     def form_valid(self, form):
-        print form.files.get('file').name.split(".")[1].upper()
-        print type (form.files.get('file'))
-        print dir(form.files.get('file'))
         form.instance.uploader = self.request.user
-        form.instance.type = form.files.get('file').name.split(".")[1].upper()
-
         return super(DocumentCreateView, self).form_valid(form)
 
 
