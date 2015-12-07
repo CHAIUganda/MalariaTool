9bc589c6732b36f62fbca91bd5a2eb7e45f7274f
Remo Samuel Paul <remosam42@gmail.com>
2015-12-07 12:39:44 +0300

    changes to tasks lisiting

--- malariatool/dashboard/models/district.py
+++ malariatool/dashboard/models/district.py
@@ -4,3 +4,7 @@ from model_utils.models import TimeStampedModel
 
 class District(TimeStampedModel):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
--- malariatool/dashboard/views/task.py
+++ malariatool/dashboard/views/task.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import CreateView, ListView
 
 from dashboard.models import Task
@@ -5,6 +6,7 @@ from dashboard.models import Task
 
 class TaskCreateView(CreateView):
     model = Task
+    success_url = reverse_lazy("dashboard:home")
     fields = ['start_date', 'end_date', 'type', 'affected_districts', 'ip']
 
 
