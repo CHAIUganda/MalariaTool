77d3c2e7256fe5e6fb716908f3e7a3e5737cc7f2
Remo Samuel Paul <remosam42@gmail.com>
2015-12-10 08:18:15 +0300

    Changes from the last meeting

--- malariatool/malariatool/settings.py
+++ malariatool/malariatool/settings.py
@@ -91,14 +91,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'PEaMjFEAj6vkEDykr4'
-EMAIL_HOST_USER = 'chaimalariatool@gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
