eb4d70d71f5ff41cd921c4929b78ca382ea4fd5d
Remo Samuel Paul <remosam42@gmail.com>
2015-12-16 11:40:22 +0300

    Added user creation email nofitications

--- malariatool/dashboard/views/users.py
+++ malariatool/dashboard/views/users.py
@@ -1,5 +1,9 @@
+from django.contrib.sites.models import Site, RequestSite
+from django.core.mail import send_mail
 from django.core.urlresolvers import reverse_lazy
+from django.template import loader
 from django.views.generic import CreateView, ListView, DeleteView
+from password_reset.utils import get_username
 
 from dashboard.forms.users import UserForm
 from dashboard.models import User
@@ -10,22 +14,37 @@ class UserCreateView(CreateView):
     model = User
     form_class = UserForm
     success_url = "/"
+    email_template_name = 'dashboard/user_email.txt'
+    email_subject_template_name = 'dashboard/user_email_subject.txt'
 
     def form_valid(self, form):
         user = form.instance
         user.set_password(form.data.get('password'))
         user.save()
         IPUserRole.assign_role_to_user(user)
-        # email = EmailMessage(
-        #     'subject_message',
-        #     'content_message',
-        #     'sender smtp gmail' + '<chaimalariatool@gmail.com>',
-        #     ['remo@codesync.ug'],
-        #     headers={'Reply-To': 'chaimalariatool@gmail.com'}
-        # )
-        #
-        # email.send(fail_silently=False)
+        self.send_notification(user, form.data.get('password'))
         return super(UserCreateView, self).form_valid(form)
+
+    def get_site(self):
+        if Site._meta.installed:
+            return Site.objects.get_current()
+        else:
+            return RequestSite(self.request)
+
+    def send_notification(self, user, password):
+        context = {
+            'user': user,
+            'site': self.get_site(),
+            'pass': password,
+            'username': get_username(user),
+            'secure': self.request.is_secure(),
+        }
+        body = loader.render_to_string(self.email_template_name,
+                                       context).strip()
+        subject = loader.render_to_string(self.email_subject_template_name,
+                                          context).strip()
+        send_mail(subject, body, "National Malaria Control<me@remosamuel.com>",
+                  [user.email])
 
 
 class UserListView(ListView):
