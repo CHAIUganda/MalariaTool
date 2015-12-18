5017a01f7787bf05c104d73b84533a85ed978fdf
Remo Samuel Paul <remosam42@gmail.com>
2015-12-18 12:01:54 +0300

    email out invites to meetings

--- malariatool/dashboard/views/meeting.py
+++ malariatool/dashboard/views/meeting.py
@@ -1,5 +1,8 @@
+from django.contrib.sites.models import Site, RequestSite
+from django.core.mail import send_mail
 from django.core.urlresolvers import reverse_lazy, reverse
-from django.views.generic import CreateView, FormView, DeleteView
+from django.template import loader
+from django.views.generic import CreateView, FormView, DeleteView, RedirectView
 
 from dashboard.forms.meeting import AddAttendee
 from dashboard.models import Meeting
@@ -48,3 +51,39 @@ class MeetingAttendeeDeleteView(DeleteView):
     def get_success_url(self):
         print self.kwargs.get('meeting_id')
         return reverse("dashboard:meeting-add-attendees", kwargs={'pk': self.kwargs.get('meeting_id')})
+
+
+class MeetingAttendeesNotifyView(RedirectView):
+    permanent = False
+    email_template_name = 'dashboard/meeting_email.txt'
+    email_subject_template_name = 'dashboard/meeting_email_subject.txt'
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse("dashboard:meeting-add-attendees", kwargs={'pk': self.kwargs.get('pk')})
+
+    def get(self, request, *args, **kwargs):
+        meeting = Meeting.objects.get(id=self.kwargs.get('pk'))
+        attendees = list(meeting.attendees.all().values_list('email', flat=True))
+        for attendee in attendees:
+            self.send_notification(attendee, meeting)
+        return super(MeetingAttendeesNotifyView, self).get(request, *args, **kwargs)
+
+    def get_site(self):
+        if Site._meta.installed:
+            return Site.objects.get_current()
+        else:
+            return RequestSite(self.request)
+
+    def send_notification(self, attendee, meeting):
+        context = {
+            'site': self.get_site(),
+            'meeting': meeting,
+            'username': attendee,
+            'secure': self.request.is_secure(),
+        }
+        body = loader.render_to_string(self.email_template_name,
+                                       context).strip()
+        subject = loader.render_to_string(self.email_subject_template_name,
+                                          context).strip()
+        send_mail(subject=subject, message=body, from_email="National Malaria Control<me@remosamuel.com>",
+                  recipient_list=[attendee],fail_silently=True)
