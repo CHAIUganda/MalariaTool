5fedbfe351550139ef0fe04623fff57ff9c5ad4c
Remo Samuel Paul <remosam42@gmail.com>
2016-01-22 08:50:38 +0300

    calendar and meeting changes

--- malariatool/dashboard/forms/meeting.py
+++ malariatool/dashboard/forms/meeting.py
@@ -13,4 +13,4 @@ class MeetingForm(ModelForm):
 class AddAttendee(forms.Form):
     ip = forms.ModelMultipleChoiceField(queryset=IP.objects.all(), help_text="Start typing out ip", required=False)
     extra_attendees = forms.CharField(widget=forms.Textarea,
-                                      help_text="Enter attendee's email addresses comma separted", required=False)
+                                      help_text="Enter attendee's email addresses separated by comma", required=False)
