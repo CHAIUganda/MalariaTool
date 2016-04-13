acd5076edba621c6b02383bf11d7ddd487be7a33
Felix Kitaka <webash.uganda@gmail.com>
2016-04-13 09:01:49 +0100

    Integrated Remo's work into the current UI.

--- malariatool/dhisdash/urls.py
+++ malariatool/dhisdash/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from dhisdash.views import HomePageView, JsonDataView, JsonDistrictsView
 
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='home'),
+    # url(r'^about/$', TemplateView.as_view(template_name='dhisdash/about-us.html'), name='about-us'),
     url(r'^data.json$', JsonDataView.as_view(), name='json_data'),
     url(r'^districts.json$', JsonDistrictsView.as_view(), name='json_data'),
 
