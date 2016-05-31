659f4423601dc70162f2c6fa46e1a8f147d86471
Malaria <malaria@ubuntu.members.linode.com>
2016-05-31 10:06:03 +0000

    new settings and gitginore

---
+++ malariatool/malariatool/local_settings.example.py
@@ -0,0 +1,10 @@
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kitutu',
+        'USER': 'kitutu',
+        'PASSWORD': 'kitutu',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
--- malariatool/malariatool/settings.py
+++ malariatool/malariatool/settings.py
@@ -109,10 +109,18 @@ DEFAULT_FROM_EMAIL = "me@remosamuel.com"
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR + "/static"
+#STATIC_ROOT = BASE_DIR + "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 
 LOGIN_REDIRECT_URL = "/"
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR + "/media/"
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
